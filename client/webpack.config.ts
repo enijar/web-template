570b4d2e492bf497d3601c4163ceabc22f6bcc1e
Enijar <TheEnijar@gmail.com>
2021-05-13 21:25:29 +0100

    chore: replace webpack js with ts

---
+++ client/webpack.config.ts
@@ -0,0 +1,87 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlInlineScriptPlugin = require("html-inline-script-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const DEV_MODE = process.env.NODE_ENV === "development";
+const SRC_DIR = path.resolve(__dirname, "src");
+const PUBLIC_DIR = path.resolve(__dirname, "public");
+const BUILD_DIR = path.resolve(__dirname, "build");
+
+module.exports = {
+  target: "browserslist",
+  stats: "minimal",
+  devtool: DEV_MODE ? "eval-cheap-module-source-map" : "source-map",
+  entry: path.join(SRC_DIR, "index.tsx"),
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "ts-loader",
+          },
+          {
+            loader: "@linaria/webpack-loader",
+            options: {
+              sourceMap: DEV_MODE,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: DEV_MODE,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(PUBLIC_DIR, "index.html"),
+      filename: "index.html",
+      inject: "body",
+    }),
+    new HtmlInlineScriptPlugin([/main.+[.]js$/]),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: PUBLIC_DIR,
+          to: BUILD_DIR,
+          globOptions: {
+            ignore: [path.join(PUBLIC_DIR, "index.html")],
+          },
+        },
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash:8].css",
+    }),
+  ],
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "[name].[contenthash:8].js",
+    chunkFilename: "[name].[contenthash:8].chunk.js",
+    path: BUILD_DIR,
+    clean: true,
+    publicPath: "/",
+  },
+};
