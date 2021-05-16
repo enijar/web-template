7c7f8416fcf2e80cd7a7f1e3156bdd77b57dc05d
Enijar <TheEnijar@gmail.com>
2021-05-16 10:38:47 +0100

    chore: add webpack config to server build

---
+++ server/webpack.config.ts
@@ -0,0 +1,43 @@
+import * as path from "path";
+const nodeExternals = require("webpack-node-externals");
+
+const DEV_MODE = process.env.NODE_ENV === "development";
+const SRC_DIR = path.resolve(__dirname, "src");
+const BUILD_DIR = path.resolve(__dirname, "build");
+
+const config = {
+  mode: DEV_MODE ? "development" : "production",
+  target: "node",
+  stats: "minimal",
+  devtool: DEV_MODE ? "eval-cheap-module-source-map" : "source-map",
+  entry: path.join(SRC_DIR, "index.ts"),
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "ts-loader",
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  externals: [nodeExternals()],
+  output: {
+    filename: "server.js",
+    path: BUILD_DIR,
+    clean: true,
+  },
+};
+
+export default () => {
+  if (!DEV_MODE) {
+    //
+  }
+  return config;
+};
