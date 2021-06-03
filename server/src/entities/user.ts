2eb1b42513993473a12e353959e8f38c8d94f214
Enijar <TheEnijar@gmail.com>
2021-06-03 19:19:26 +0100

    chore: remove unused user entity import

--- server/src/entities/user.ts
+++ server/src/entities/user.ts
@@ -1,4 +1,4 @@
-import { Column, Index, Model, Table, Unique } from "sequelize-typescript";
+import { Column, Index, Model, Table } from "sequelize-typescript";
 
 @Table({ tableName: "users" })
 export default class User extends Model {
