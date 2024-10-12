9a36cc563869d3a554d3df3f6d0dcf0994e698b4
Enijar <TheEnijar@gmail.com>
2024-10-12 15:34:53 +0100

    chore: revert client from vite to webpack

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 const config = {
-  apiUrl: import.meta.env.VITE_API_URL ?? "http://localhost:3000",
+  apiUrl: process.env.API_URL ?? "http://localhost:3000",
 } as const;
 
 export default config;
