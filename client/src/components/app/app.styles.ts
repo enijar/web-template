972a8217d5038ca35636e813b7b75ee591a332f3
Enijar <TheEnijar@gmail.com>
2024-12-08 17:09:53 +0000

    chore: clean up reset styles

--- client/src/components/app/app.styles.ts
+++ client/src/components/app/app.styles.ts
@@ -1,48 +1,26 @@
 import { createGlobalStyle } from "styled-components";
 
 export const AppReset = createGlobalStyle`
-  :root {
-    --root-scale: 3;
-    --root-size: calc(var(--root-scale) * 1vh);
-    --root-min: 16px;
-    --root-max: 20px;
-  }
-
-  @media (orientation: portrait) {
-    --root-size: calc(var(--root-scale) * 1vw);
-  }
-
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: inherit;
     font-size: inherit;
+    font-family: inherit;
     font-weight: inherit;
     line-height: inherit;
-    color: inherit;
-  }
-
-  html,
-  body,
-  #root,
-  main {
-    width: 100%;
-    height: 100%;
+    text-rendering: inherit;
   }
 
   html {
-    font-size: clamp(var(--root-min), var(--root-size), var(--root-max));
+    font-size: 100%;
     font-family: system-ui, sans-serif;
-    font-weight: normal;
-    line-height: 1;
+    line-height: normal;
     text-rendering: geometricPrecision;
-    background-color: #1e1e1e;
-    color: #fefefe;
+    color-scheme: light dark;
   }
 
-  main {
-    overflow: auto;
-    -webkit-overflow-scrolling: touch;
+  body {
+    overscroll-behavior: none;
   }
 `;
