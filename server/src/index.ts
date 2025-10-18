31b842d909bdc5891af851827e558fb4f5650a03
Enijar <TheEnijar@gmail.com>
2025-10-18 13:58:21 +0100

    chore: clean up server entrypoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,18 +3,16 @@ import config from "~/config.js";
 import database from "~/services/database.js";
 import app from "~/services/app.js";
 
-database
-  .sync({ alter: true })
-  .then(() => {
-    const server = serve({ ...app, port: config.port });
-    server.on("error", (err) => {
-      console.error(err);
-    });
-    server.on("listening", async () => {
-      console.log(`Server running: http://localhost:${config.port}`);
-    });
-  })
-  .catch((err) => {
+try {
+  await database.sync({ alter: true });
+  const server = serve({ ...app, port: config.port });
+  server.on("error", (err) => {
     console.error(err);
-    process.exit(1);
   });
+  server.on("listening", async () => {
+    console.log(`Server running: http://localhost:${config.port}`);
+  });
+} catch (err) {
+  console.error(err);
+  process.exit(1);
+}
