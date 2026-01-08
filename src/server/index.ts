b49e9aed9cd851cb07d6e6e9af0aaa51716b76e6
Enijar <TheEnijar@gmail.com>
2026-01-08 16:07:39 +0000

    fix(server): user config port

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,15 +1,16 @@
 import { serve } from "@hono/node-server";
 import database from "server/services/database.js";
 import app from "server/services/app.js";
+import config from "config/index.js";
 
 try {
   await database.sync({ alter: true });
-  const server = serve({ ...app, port: 3000 });
+  const server = serve({ ...app, port: config.PORT });
   server.on("error", (err) => {
     console.error(err);
   });
   server.on("listening", async () => {
-    console.log(`Server running: http://localhost:${3000}`);
+    console.log(`Server running: http://localhost:${config.PORT}`);
   });
 } catch (err) {
   console.error(err);
